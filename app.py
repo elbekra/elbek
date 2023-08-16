9b871d01ee861736756d96d1dbbcf28e22bf751b
elbekra <142199805+elbekra@users.noreply.github.com>
2023-08-16 10:36:57 +0500

    app.py
    elbek\

---
+++ app.py
@@ -0,0 +1,84 @@
+# print("hello world",end=",")
+# print("salom dunyo",end=",")
+# print("alijon valijonov")
+# print(150**15)
+# print("Alijon\nValijonovo'og'li")
+# print('Salom Alijono\'g\'li')
+# print('''Salomlar
+# Alijon
+# Valijon
+# O'g'li''')
+# print("""shamsbek aka qandaysiv""")
+
+# name="Elbek"
+# familiya="Radjabov"
+# print(f"assalomu aleykum {name} {familiya}")
+# ism=input("Ismingizni kiriting:")
+# familiya=input("familangizni kiriting:")
+# print(f"Assalomu alaykum {ism} {familiya}")
+# ism=input("Ismingizni kiriting:")
+# familiya=input("Familiyangizni kiriting:")
+# shrif=input("Sharifingzni kiriting:")
+# print(f"Sizni to'liq ism sharifingiz {ism} {familiya} {shrif}")
+# a=int(input("a sonni kiriting:"))
+# b=int(input("b sonni kiriting:"))
+# print(a+b)
+# tyil=int(input("tug'ulgan yilingizni kiriting:"))
+# yil=int(input("hozirgi yilni kiriting:"))
+# print(f"sizning yoshingiz {yil-tyil}da")
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+# qoldiq = a % b
+# print("a ni b ga bo'lgandagi qoaldiq",qoldiq)
+
+# x = 15
+# y = 12
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x ** y)
+# print(x // y)
+# print(x % y)
+
+
+# a = True
+# b = False
+#
+# print(not (a and b or a or a))
+
+# a = 10
+# b = 10
+#
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+
+
+
+# a = [1, 2, 3]
+# b = a
+# print(a is b)
+# c = 10
+# x = 10
+# print(c is x)
+#
+# a = 10pythonProject
+# b = 15.5
+# print(type(b))
+
+
+# a = 1
+# b = 0
+# print(bool(a))
+
+# a = 15
+# b = 15000
+# print(a is not b)
+
+
+# name = ['ali','vail','qosim','nurbek','valecher','elbek']
+# print(name[::-2])
+
